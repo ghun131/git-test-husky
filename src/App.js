de06cde2cdb83b41b3d5ad36abafba794fb3e7b3
seta-hungDang <hung.dang@seta-international.vn>
2019-06-22 18:26:37 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>hey hey hey</h2>
       </header>
+      <main>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+          hendrerit ex ac condimentum facilisis. Pellentesque interdum fringilla
+          leo. Maecenas finibus aliquet ligula, id porttitor massa ullamcorper
+          nec. Proin non massa eu lorem maximus consectetur. Phasellus nec
+          vehicula metus. Nunc vel augue fringilla, dapibus tortor porttitor,
+          porttitor ex. Praesent eleifend sem eu risus finibus, et lacinia
+          lectus scelerisque. Morbi aliquam enim quis maximus porttitor. Etiam
+          id neque et est mollis vulputate. In quam nulla, consectetur nec
+          lobortis ac, scelerisque a diam.
+        </p>
+        <p>
+          Sed nec lacinia turpis. Vivamus maximus mollis neque vel viverra.
+          Integer sed consequat urna, in cursus lorem. Suspendisse luctus rutrum
+          mi, eu iaculis quam posuere ac. Fusce auctor mauris metus, tincidunt
+          accumsan mauris bibendum eget. Nullam fermentum diam ut arcu posuere,
+          vel bibendum nisl euismod. Duis tempus erat in elit varius, a molestie
+          tellus tempus. Nunc et nunc purus. Vestibulum imperdiet lacus enim, et
+          pellentesque velit pellentesque id. Quisque tristique enim elit, id
+          facilisis lorem fermentum sed. Curabitur ipsum nunc, tincidunt a
+          mollis sed, dictum in tortor. Donec non dignissim tellus. Donec velit
+          dolor, elementum ultrices libero rutrum, eleifend egestas urna.
+        </p>
+        <p>
+          Morbi pharetra, magna quis molestie pellentesque, lorem magna iaculis
+          diam, quis mollis diam ante sed arcu. Cras sit amet massa elit.
+          Aliquam erat volutpat. Fusce bibendum diam at turpis tempor aliquet.
+          Fusce in mi quam. Nulla facilisi. Phasellus at interdum est. Morbi
+          vehicula a orci non tempus. Sed id lectus id libero porta commodo at
+          blandit magna. Morbi fermentum a mi sed venenatis.
+        </p>
+      </main>
+      <footer>
+        Copyright &#169; by <strong>Hung</strong>. All rights reserved.
+      </footer>
     </div>
   );
 }
